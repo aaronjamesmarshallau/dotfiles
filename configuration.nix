0a25c6d59617313881de721c1484c22cc8256716
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2025-06-29 14:45:12 +1000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,181 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let unstable = import <nixos-unstable> {
+  config = {
+    allowUnfree = true;
+  };
+};
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./home.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "nvidia_drm.fbdev=1" "nvidia_drm.modeset=1"];
+
+  boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Enable SDDM for login
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  # Auto Login because hyprlock is the way
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "amarshall";
+  };
+
+  hardware.xpadneo.enable = true;
+
+  # nvidia
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Required
+    modesetting.enable = true;
+
+    # By default, only the bare essentials are written to disk from
+    # VRAM. If you encounter video corruptions after waking from sleep
+    # try enabling this option.
+    powerManagement.enable = false;
+
+    # Turns off GPU when not in use - only supported by Turing or newer
+    # cards.
+    powerManagement.finegrained = false;
+
+    # Use the open source kernel for your GPU. Not to be confused with
+    # nouveau drivers
+    open = false;
+
+    # nvidia settings UI enabled (`nvidia-settings`)
+    nvidiaSettings = true;
+
+    # Specify which drivers to use.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Configure keymap in X11
+  #services.xserver.xkb = {
+  #  layout = "us";
+  #  variant = "";
+  #};
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enables stuff for the file manager
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+
+  # Keyring
+  services.gnome.gnome-keyring.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.amarshall = {
+    isNormalUser = true;
+    description = "Aaron Marshall";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.symbols-only
+    nerd-fonts.fantasque-sans-mono
+    cabin
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
---
+++ home.nix
@@ -0,0 +1,155 @@
+{ config, pkgs, lib, ... }:
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+  };
+  unstable = import <nixos-unstable> {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "openssl-1.1.1w"
+      ];
+    };
+  };
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
+  users.users.amarshall.extraGroups = [ "docker" ];
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Development port
+  networking.firewall.allowedTCPPorts = [ 5173 ];
+  networking.hosts = lib.mkForce {
+    "127.0.0.1" = [ "nixos" ];
+  };
+
+  home-manager.users.amarshall = {
+    # This should be the same value as `system.stateVersion` in
+    # your `configuration.nix` file.
+    home.stateVersion = "24.11";
+
+    programs.zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = false;
+      enableNushellIntegration = false;
+      enableZshIntegration = false;
+    };
+
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+      case "$TERM" in
+          xterm-color|*-256color|*ghostty) color_prompt=yes;;
+      esac
+      '';
+    };
+
+    programs.java = {
+      enable = true;
+      package = pkgs.temurin-bin;
+    };
+
+    # Install Firefox
+    programs.firefox.enable = true;
+
+    # Install Waybar
+    programs.waybar.enable = true;
+
+    home.file = {            
+      # other configs
+      ".config" = {
+        source = ./src/configs;
+        recursive = true;
+      };
+    };
+
+    home.sessionVariables = {
+      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    };
+
+    home.pointerCursor = {
+      name = "catppuccin-mocha-dark-cursors";
+      package = pkgs.catppuccin-cursors.mochaDark;
+      size = 24;
+      gtk.enable = true;
+    };
+
+    home.packages = [
+      pkgs.awscli2                    # awscli2
+      pkgs.curl                       # Curl
+      pkgs.davinci-resolve            # Video editing
+      pkgs.dig                        # dig
+      pkgs.evtest                     # Evtest
+      pkgs.fd                         # fd - find but better
+      pkgs.feh                        # Image Viewer
+      pkgs.gamescope                  # Gamescope for Wayland
+      pkgs.ghostty                    # Ghostty terminal
+      pkgs.gimp                       # Image editor
+      pkgs.git                        # Git
+      pkgs.godot_4                    # Godot game engine
+      pkgs.jetbrains.idea-community   # Intellij IDEA IDE
+      pkgs.lutris                     # Lutris game manager
+      pkgs.mpv                        # media player
+      pkgs.nodejs                     # NodeJS
+      pkgs.postgresql_17              # Postgres
+      pkgs.prismlauncher              # Minecraft launcher
+      pkgs.solaar                     # Solaar Logitech Device Manager
+      pkgs.spotify                    # Spotify
+      pkgs.sysfsutils                 # Systool
+      pkgs.usbutils                   # lsusb
+      pkgs.vesktop                    # Custom discord client
+      pkgs.vscode                     # Visual Studio Code
+      pkgs.wev                        # Wayland Events
+      pkgs.wget                       # Wget
+      pkgs.xfce.xfconf                # XFConf configuration system
+      pkgs.xivlauncher                # XIVLauncher for FFXIV
+      pkgs.zip                        # ZIP package
+
+      # Thunar file manager
+      (pkgs.xfce.thunar.override {
+        thunarPlugins = with pkgs.xfce; [
+          thunar-archive-plugin
+          thunar-volman
+        ];
+      })
+
+      # Flameshot screenshot tool
+      (pkgs.flameshot.override { enableWlrSupport = true; })
+
+      # System packages
+      pkgs.cliphist                   # Command line history manager
+      pkgs.coreutils                  # Core utilities
+      pkgs.egl-wayland                # Required for nvidia/wayland/compositor compatibility
+      pkgs.font-manager               # Font manager GUI
+      pkgs.kdePackages.ark            # Archive manager
+      pkgs.mako                       # Notification daemon
+      pkgs.myxer                      # Audio mixer GUI
+      pkgs.rofi-wayland               # Rofi general purpose list tool
+      pkgs.seahorse                   # Seahorse password manager
+      pkgs.zenity                     # Zenity dialog tool
+
+      unstable.bruno                  # Bruno rest client
+      unstable.hyprlock
+      unstable.signal-desktop         # Signal encrypted messenger
+      unstable.wpaperd                # Wallpaper configuration
+      (unstable.bolt-launcher.override { enableRS3 = true; })
+    ];
+
+    gtk.cursorTheme = {
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "catppuccin-mocha-dark-cursors";
+    };
+  };
+}
